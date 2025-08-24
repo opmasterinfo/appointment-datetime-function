41c793fe816a33303d51e057805382e1363ae7f4
opmasterinfo <haseebinfo607@gmail.com>
2025-08-24 04:49:09 +0430

    Update format-datetime.js

--- netlify/functions/format-datetime.js
+++ netlify/functions/format-datetime.js
@@ -1,21 +1,23 @@
 exports.handler = async (event) => {
-  const { current_datetime, appointment_datetime, appointment_duration_in_minutes } = JSON.parse(event.body);
+  const { appointment_datetime, appointment_duration_in_minutes } = JSON.parse(event.body);
 
   const durationInMinutes = parseInt(appointment_duration_in_minutes);
-
+  
   let appointmentDateTime, appointmentDate, appointmentTime, timeMin, timeMax;
+
+  // The timezone is always -04:00 for New York
+  const fixedTimezoneOffset = '-04:00';
 
   // Check if the appointment datetime string ends with 'T00:00:00' to identify a date-only input.
   if (appointment_datetime.endsWith('T00:00:00')) {
     // Case 2: appointment_datetime is effectively a date-only input
     const appointmentDateOnly = appointment_datetime.split('T')[0];
-    const timezone = current_datetime.slice(current_datetime.indexOf('T') + 1);
 
-    appointmentDateTime = `${appointmentDateOnly}T00:00:00${timezone}`;
+    appointmentDateTime = `${appointmentDateOnly}T00:00:00${fixedTimezoneOffset}`;
     appointmentDate = appointmentDateOnly;
     appointmentTime = "";
-    timeMin = `${appointmentDateOnly}T00:00:00${timezone}`;
-    timeMax = `${appointmentDateOnly}T23:59:59${timezone}`;
+    timeMin = `${appointmentDateOnly}T00:00:00${fixedTimezoneOffset}`;
+    timeMax = `${appointmentDateOnly}T23:59:59${fixedTimezoneOffset}`;
 
   } else {
     // Case 1: appointment_datetime includes a specific time and timezone
